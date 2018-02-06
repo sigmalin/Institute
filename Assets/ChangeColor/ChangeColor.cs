a4bc48526ab1cb1d688dfa768c688c32d54a7b79
sigmalin <noirland@hotmail.com>
2018-02-06 14:40:33 +0800

    Color Change
    RGB -> HSV
    Alpha for mask

---
+++ Assets/ChangeColor/ChangeColor.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour 
+{
+	public Texture2D FromHSV;
+	public Material Target;
+
+	Texture2D ToRGB;
+
+	// Use this for initialization
+	void Start () 
+	{
+		ToRGB = new Texture2D (FromHSV.width, FromHSV.height, TextureFormat.ARGB32, false);
+
+		OnChange ();
+	}
+
+	void OnDestroy()
+	{
+		if (ToRGB != null) 
+		{
+			Destroy (ToRGB);
+			ToRGB = null;
+		}
+	}
+
+	float hSliderValue = 0f;
+	void OnGUI()
+	{
+		hSliderValue = GUI.HorizontalSlider (new Rect(20,20,359,30), hSliderValue, 0f, 1f);
+		if (GUI.Button (new Rect (20, 80, 60, 30), "Change"))
+			OnChange();
+	}
+
+	void OnChange()
+	{
+		Color[] src = FromHSV.GetPixels();
+
+		for(int Indx = 0; Indx < src.Length; ++Indx)
+		{
+			InvTransHSV (ref src[Indx], hSliderValue);
+		}
+
+		ToRGB.SetPixels(src);
+		ToRGB.Apply();
+
+		Target.mainTexture = ToRGB;
+	}
+
+	void InvTransHSV(ref Color _color, float _diffHue)
+	{
+		float h = _color.r;
+
+		if (0f < _color.a) 
+		{
+			h += _diffHue;
+			h -= Mathf.Floor (h);
+		}
+
+		_color = Color.HSVToRGB (h, _color.g, _color.b);
+	}
+}
---
+++ Assets/ChangeColor/RGB2HSV.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+///
+// Alpha for Color Change Mask
+///
+public class RGB2HSC : MonoBehaviour 
+{
+	public Texture2D SrcRGB;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (SrcRGB == null)
+			return;
+
+		int width = SrcRGB.width;
+		int height = SrcRGB.height;
+
+		Texture2D resTexture = new Texture2D (width, height, TextureFormat.ARGB32, false);
+
+		Color[] cols = SrcRGB.GetPixels();
+
+		for(int Indx = 0; Indx < cols.Length; ++Indx)
+			RGB2HSV(ref cols[Indx]);
+
+		resTexture.SetPixels(cols);
+		resTexture.Apply();
+
+		//save to file
+		//System.IO.File.WriteAllBytes ("xxx.png", resTexture.EncodeToPNG());
+	}
+
+	void RGB2HSV(ref Color _col)
+	{
+		float h = 0f;
+		float s = 0f;
+		float v = 0f;
+
+		Color.RGBToHSV (_col, out h, out s, out v);
+
+		_col.r = h;
+		_col.g = s;
+		_col.b = v;
+	}
+}

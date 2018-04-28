367f3880013cbdeeabb6cd4e38d890d0b26b36da
sigmalin <noirland@hotmail.com>
2018-04-28 11:37:52 +0800

    VolumetricCloud
    VolumetricCloud

---
+++ Assets/VolumetricCloud/Shaders/VolumetricCloud.shader
@@ -0,0 +1,114 @@
+﻿Shader "VolumetricCloud/VolumetricCloud"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_Color ("Cloud Top Color", Color) = (1,1,1,1)
+		_ColorB ("Cloud Botton Color", Color) = (1,1,1,1)
+		_Factor ("Alpha Factor", Range(0,1)) = 0.4
+		_Wind ("Wind", Vector) = (1,0,0,1)
+	}
+	SubShader
+	{
+		Tags { "Queue"="Overlay" "IgnoreProjector"="True" "RenderType"="Transparent" }
+		ZWrite Off
+		Cull Off
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float4 texcoord : TEXCOORD0;
+				float4 texcoord2 : TEXCOORD1;
+			};
+
+			struct v2f
+			{
+				half4 pos : SV_POSITION;
+				half2 texcoord : TEXCOORD0;
+				fixed4 color : COLOR;
+			};
+
+			sampler2D _MainTex;
+			fixed4 _Color;
+			fixed4 _ColorB;
+			fixed _Factor;
+
+			fixed4 _Wind;
+
+			float  _CurrentTime;
+			float  _LifeTime;
+			
+			v2f vert (appdata v)
+			{
+				float elapsed = _CurrentTime - v.texcoord2.w;
+
+				float4 tv = v.vertex;
+				tv.xyz += _Wind.xyz * elapsed;
+
+				half sizeX = v.texcoord2.x;
+				half sizeY = v.texcoord2.y;
+/*								
+				half3 side = UNITY_MATRIX_IT_MV[1].xyz;//UNITY_MATRIX_V[1].xyz;//
+				half3 eye = normalize(ObjSpaceViewDir(tv));
+				half3 up = cross(side, eye);
+*/					
+				half3 eye = normalize(ObjSpaceViewDir(tv));
+				half3 up = UNITY_MATRIX_V[0].xyz;//UNITY_MATRIX_IT_MV[0].xyz
+				half3 side = cross(eye, up);
+	
+				// rotate
+				half3 vec = (v.texcoord.x)*side*sizeX + (v.texcoord.y)*up*sizeY;
+				half3 n = eye;
+				half theta = v.texcoord2.z;
+				/* rotate matrix for an arbitrary axis
+				 * Vx*Vx*(1-cos) + cos  	Vx*Vy*(1-cos) - Vz*sin	Vz*Vx*(1-cos) + Vy*sin;
+				 * Vx*Vy*(1-cos) + Vz*sin	Vy*Vy*(1-cos) + cos 	Vy*Vz*(1-cos) - Vx*sin;
+				 * Vz*Vx*(1-cos) - Vy*sin	Vy*Vz*(1-cos) + Vx*sin	Vz*Vz*(1-cos) + cos;
+				 */
+				half s, c;
+				sincos(theta, s, c);
+				half3 n1c = n * (1-c);
+				half3 ns = n * s;
+				half3x3 mat = {
+					(n.x*n1c.x + c),   (n.x*n1c.y - ns.z), (n.z*n1c.x + ns.y),
+					(n.x*n1c.y + ns.z), (n.y*n1c.y + c),   (n.y*n1c.z - ns.x),
+					(n.z*n1c.x - ns.y), (n.y*n1c.z + ns.x),   (n.z*n1c.z + c),
+				};
+				half3 rvec = mul(mat, vec);
+				tv.xyz += rvec;
+
+            	v2f o;
+			    o.pos = mul(UNITY_MATRIX_VP, float4(tv.xyz,1));//UnityObjectToClipPos(float4(tv.xyz,1));
+				o.texcoord = MultiplyUV(UNITY_MATRIX_TEXTURE0,
+										float4(v.texcoord.xy, 0, 0)) + half2(0.5,0.5);
+
+				
+				o.color = lerp(_ColorB, _Color, ((rvec.y + sizeY*0.5)/(sizeY)));
+
+				fixed halfLife = _LifeTime * 0.5;
+				halfLife = abs((halfLife - elapsed) / halfLife);
+				o.color.a = 1 - saturate(halfLife - 0.9) * 10;
+				
+            	return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.texcoord);
+				fixed a = col.a * _Factor * i.color.a;
+				//return fixed4(_Color.rgb,a);
+				return fixed4(i.color.rgb,a);
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/VolumetricCloud/VolumetricCloud.cs
@@ -0,0 +1,249 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class VolumetricCloud : MonoBehaviour 
+{
+
+	Mesh mMesh;
+	[SerializeField] Material mMaterial;
+
+	const int CLOUD_MAX = 512;
+
+	const float CLOUD_LIFE = 2f;
+
+	const float CLOUD_INTERVAL = 2f;
+
+	List<Vector3> mVertices;
+	List<Vector2> mUVs;
+	List<Vector4> mUV2s;
+	List<int> mIndices;
+
+	protected class CloudData
+	{
+		public Vector3 Pos { get; set; }
+		public float ScaleX { get; set; }
+		public float ScaleY { get; set; }
+		public float Rotate { get; set; }
+		public float Time { get; set; }
+	}
+
+	Queue<CloudData> mCloudDatas;
+	Stack<CloudData> mFreeCloudData;
+
+	float mCurTime;
+	bool mRefresh;
+
+	float mLastTime;
+
+	Vector3 mCenter;
+
+	readonly Vector2[] UV_MAPS = new Vector2[] {
+		new Vector2(-1.077f, -0.5f),
+		new Vector2(     0f,  1.366f),
+		new Vector2( 1.077f, -0.5f),
+	};
+
+	readonly float PI_2 = Mathf.PI * 2f;
+	int ShaderPropertyCurrentTime;
+	int ShaderPropertyLifeTime;
+
+	// Use this for initialization
+	public void Initialize ()
+	{
+		mMesh = new Mesh();
+		mMesh.name = "CloudMesh";
+		mMesh.MarkDynamic();
+
+		mVertices = new List<Vector3>(CLOUD_MAX*3);
+		mUVs      = new List<Vector2>(CLOUD_MAX*3);
+		mUV2s     = new List<Vector4>(CLOUD_MAX*3);
+		mIndices  = new List<int>(CLOUD_MAX*3);
+
+		mCloudDatas = new Queue<CloudData>(CLOUD_MAX);
+		mFreeCloudData = new Stack<CloudData>(CLOUD_MAX);
+		for(int Indx = 0; Indx < CLOUD_MAX; ++Indx)
+		{
+			mFreeCloudData.Push(new CloudData());
+		}
+
+		mRefresh = false;
+
+		this.GetComponent<MeshFilter>().sharedMesh = mMesh;	
+		this.GetComponent<MeshRenderer>().sharedMaterial = mMaterial;
+
+		ShaderPropertyCurrentTime = Shader.PropertyToID("_CurrentTime");
+
+		ShaderPropertyLifeTime = Shader.PropertyToID("_LifeTime");
+		mMaterial.SetFloat(ShaderPropertyLifeTime, CLOUD_LIFE);
+	}
+
+	void UpdateCloudDatas()
+	{
+		while(mCloudDatas.Count != 0)
+		{
+			if(mCurTime - mCloudDatas.Peek().Time < CLOUD_LIFE)
+				break;
+
+			mFreeCloudData.Push(mCloudDatas.Dequeue());
+			mRefresh = true;
+		}
+	}
+
+	void CreateMesh()
+	{
+		if(mRefresh == false)
+			return;
+
+		ClearMeshData();
+
+		int dataIndx = 0;
+
+		Queue<CloudData>.Enumerator etor = mCloudDatas.GetEnumerator();		
+		while(etor.MoveNext())
+		{
+			CloudData data = etor.Current;
+
+			for(int Indx = 0; Indx < 3; ++Indx)
+			{
+				mVertices.Add(data.Pos);
+				mUVs.Add(UV_MAPS[Indx]);
+				mUV2s.Add(new Vector4(data.ScaleX, data.ScaleY, data.Rotate, data.Time));
+			}
+
+			mIndices.Add(dataIndx*3+0);
+			mIndices.Add(dataIndx*3+1);
+			mIndices.Add(dataIndx*3+2);
+
+			++dataIndx;			
+		}
+
+		mMesh.Clear();
+
+		mMesh.SetVertices(mVertices);
+		mMesh.SetUVs(0, mUVs);
+		mMesh.SetUVs(1, mUV2s);
+		mMesh.SetTriangles(mIndices, 0, false);
+
+		mMesh.bounds = new Bounds(Vector3.zero, Vector3.one * 99999999f);
+
+		mRefresh = false;
+	}
+
+	void ClearMeshData()
+	{
+		mVertices.Clear();
+		mUVs.Clear();
+		mUV2s.Clear();
+		mIndices.Clear();
+	}
+
+	void Add(ref Vector3 _pos)
+	{
+		if(mFreeCloudData.Count == 0)
+			return;
+
+		CloudData data = mFreeCloudData.Pop();
+		data.Pos = _pos;
+		float scale = Random.Range(4f, 8f);
+		data.ScaleX = scale;
+		data.ScaleY = scale;
+		data.Rotate = Random.Range(0, PI_2);
+		data.Time = mCurTime;
+
+		mCloudDatas.Enqueue(data);
+
+		mRefresh = true;
+	}
+
+	void AddCloud(ref Vector3 _pos)
+	{
+		int countGroup = 64;//Random.Range(16, 32);
+		List<Vector3> clouds = new List<Vector3>(countGroup);
+
+		Vector3 center = Vector3.one * 0.5f;
+		Vector3 meshScale = new Vector3(10f,4f,10f);
+
+		for(int Indx = 0; Indx < countGroup; ++Indx)
+		{
+			clouds.Add(_pos + Vector3.Scale (new Vector3(Random.value,Random.value,Random.value) - center, meshScale));
+		}
+
+		clouds.Sort(CompareCloud);
+
+		for(int Indx = 0; Indx < countGroup; ++Indx)
+		{
+			Vector3 addr = clouds[Indx];
+			Add(ref addr);
+		}
+	}
+
+	int CompareCloud(Vector3 _1, Vector3 _2)
+	{
+		if(_2.z < _1.z)
+			return -1;
+		if(_1.z < _2.z)
+			return 1;
+		return 0;
+	}
+
+	void CreateCloud()
+	{
+		if(mCurTime - mLastTime < CLOUD_INTERVAL)
+			return;
+
+		mLastTime = mCurTime;
+
+		float length = 0f;
+		float angle = Random.Range(0f,2f) * Mathf.PI;
+
+		Vector3 center = mCenter + (Vector3.right * Mathf.Cos(angle) + Vector3.forward * Mathf.Sin(angle)) * length + Vector3.up * 8f;
+		
+		AddCloud(ref center);
+	}
+
+	public void UpdateCenter(ref Vector3 _center)
+	{
+		mCenter = _center;
+	}
+
+	public void Framemove()
+	{
+		mCurTime = Time.realtimeSinceStartup;
+
+		CreateCloud();
+
+		UpdateCloudDatas();
+		CreateMesh();
+		SettingMaterial();
+	}
+
+	void SettingMaterial()
+	{
+		if(mMaterial == null)
+			return;
+
+		mMaterial.SetFloat(ShaderPropertyCurrentTime, mCurTime);
+	}
+
+#region DEMO
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		Initialize();
+	}
+
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		Framemove();
+	}
+#endregion
+}

ab75f9aed93ef1f0a3705befe19bc4d78727530a
sigmalin <noirland@hotmail.com>
2022-01-17 11:13:40 +0800

    Update and rename IGpuDrivenUnit.cs to IGpuDrivenTerrain.cs

--- Assets/GpuDrivenTerrain/IGpuDrivenTerrain.cs
+++ Assets/GpuDrivenTerrain/IGpuDrivenTerrain.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public interface IGpuDrivenUnit
+public interface IGpuDrivenTerrain
 {
+    bool onCulling(CommandBuffer _cmd, Camera _cam);
     bool onRender(CommandBuffer _cmd);
 }

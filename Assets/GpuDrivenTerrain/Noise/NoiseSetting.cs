35ef9f24e889b4fa89e8ef9226d56601485adc19
sigmalin <noirland@hotmail.com>
2022-01-24 11:00:43 +0800

    Create NoiseSetting.cs

---
+++ Assets/GpuDrivenTerrain/Noise/NoiseSetting.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseSetting
+{
+    [HideInInspector]
+    public readonly int SimplexNoiseSize = 2048;
+
+    [HideInInspector]
+    public readonly int SimplexNoiseMask = 2047;
+
+    public long NoiseSeed = 114514;
+}

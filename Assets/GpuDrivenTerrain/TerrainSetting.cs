c732ff7f0583684e935a80ed2b6d2bb559e0d7c3
sigmalin <noirland@hotmail.com>
2022-01-24 13:35:58 +0800

    Create TerrainSetting.cs

---
+++ Assets/GpuDrivenTerrain/TerrainSetting.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TerrainSetting
+{
+    public QuadTreeSetting QuadTree;
+
+    public NoiseSetting Noise;
+
+    public Material matTerrain;
+}

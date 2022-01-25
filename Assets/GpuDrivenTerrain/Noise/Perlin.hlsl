b87f130f640fe7c55c80c68f1a7df85c210d7b88
sigmalin <noirland@hotmail.com>
2022-01-25 14:37:40 +0800

    Update Perlin.hlsl

--- Assets/GpuDrivenTerrain/Noise/Perlin.hlsl
+++ Assets/GpuDrivenTerrain/Noise/Perlin.hlsl
@@ -19,7 +19,40 @@ float4 GetGrad(int2 st)
     return float4(((perm >> 24) & 0x03) - 1.0, ((perm >> 16) & 0x03) - 1.0, ((perm >> 8) & 0x03) - 1.0, (perm & 0x03) - 1.0);
 }
 
-float3 PerlinNoise(float2 p, float seed)
+float perlinNoise(float2 p, float seed)
+{
+    // Calculate 2D integer coordinates i and fraction p.
+    float2 i = floor(p);
+    float2 f = p - i;
+
+    // Get weights from the coordinate fraction
+    float2 w = f * f * f * (f * (f * 6 - 15) + 10);
+    float4 w4 = float4(1, w.x, w.y, w.x * w.y);
+
+    // Get the four randomly permutated indices from the noise lattice nearest to
+    // p and offset these numbers with the seed number.
+    float4 perm = GetPerm(i) + seed;
+
+    // Permutate the four offseted indices again and get the 2D gradient for each
+    // of the four permutated coordinates-seed pairs.
+    float4 g1 = GetGrad(perm.xy);
+    float4 g2 = GetGrad(perm.zw);
+
+    // Evaluate the four lattice gradients at p
+    float a = dot(g1.xy, f);
+    float b = dot(g2.xy, f + float2(-1, 0));
+    float c = dot(g1.zw, f + float2(0, -1));
+    float d = dot(g2.zw, f + float2(-1, -1));
+
+    // Bi-linearly blend between the gradients, using w4 as blend factors.
+    float4 grads = float4(a, b - a, c - a, a - b - c + d);
+    float n = dot(grads, w4);
+
+    // Return the noise value, roughly normalized in the range [-1, 1]
+    return n * 1.5;
+}
+
+float3 perlinNoisePseudoDeriv(float2 p, float seed)
 {
     // Calculate 2D integer coordinates i and fraction p.
     float2 i = floor(p);
@@ -57,44 +90,66 @@ float3 PerlinNoise(float2 p, float seed)
 
     // Return the noise value, roughly normalized in the range [-1, 1]
     // Also return the pseudo dn/dx and dn/dy, scaled by the same factor
-    //return float3(n, dx, dy) * 1.5;
-    return float3(n, dx, dy);
+    return float3(n, dx, dy) * 1.5;
 }
 
 float turbulence(float2 p, float seed, int octaves,
-    float lacunarity = 2.0, float gain = 0.5)
+    float amp = 1000.0, float freq = 0.0005, float lacunarity = 2.0, float gain = 0.5)
 {
     float sum = 0;
-    float freq = 1.0, amp = 1.0;
-    float maxAmp = 0;
     for (int i = 0; i < octaves; i++)
     {
-        float n = PerlinNoise(p * freq, seed + i / 256.0);
+        float n = perlinNoise(p * freq, seed + i / 256.0);
         sum += n * amp;
         freq *= lacunarity;
-        maxAmp += amp;
         amp *= gain;
     }
-    return sum / maxAmp;
+    return sum;
 }
 
-float iqTurbulence(float2 p, float seed, int octaves,
-    float lacunarity = 2.0, float gain = 0.5)
+float billowedTurbulence(float2 p, float seed, int octaves,
+    float amp = 1000.0, float freq = 0.0005, float lacunarity = 2.0, float gain = 0.5)
 {
-    float sum = 0;// 0.5;
-    float freq = 1.0, amp = 1.0;
-    float2 dsum = float2(0, 0);
-    float maxAmp = 0;
+    float sum = 0;
     for (int i = 0; i < octaves; i++)
     {
-        float3 n = PerlinNoise(p * freq, seed + i / 256.0);
+        float n = abs(perlinNoise(p * freq, seed + i / 256.0));
+        sum += n * amp;
+        freq *= lacunarity;
+        amp *= gain;
+    }
+    return sum;
+}
+
+float RidgedTurbulence(float2 p, float seed, int octaves,
+    float amp = 1000.0, float freq = 0.0005, float lacunarity = 2.0, float gain = 0.5)
+{
+    float sum = 0;
+    for (int i = 0; i < octaves; i++)
+    {
+        float n = 1 - abs(perlinNoise(p * freq, seed + i / 256.0));
+        sum += n * amp;
+        freq *= lacunarity;
+        amp *= gain;
+    }
+    return sum;
+}
+
+
+float iqTurbulence(float2 p, float seed, int octaves,
+    float amp = 1000.0, float freq = 0.0005, float lacunarity = 2.0, float gain = 0.5)
+{
+    float sum = 0;
+    float2 dsum = float2(0, 0);
+    for (int i = 0; i < octaves; i++)
+    {
+        float3 n = perlinNoisePseudoDeriv(p * freq, seed + i / 256.0);
         dsum += n.yz;
         sum += amp * n.x / (1 + dot(dsum, dsum));
         freq *= lacunarity;
-        maxAmp += amp;
-        amp *= gain;        
+        amp *= gain;
     }
-    return sum / maxAmp;
+    return sum;
 }
 
 #endif
